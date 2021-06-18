976a583d1cad3374e9e82f9bb4097b433381dfb2
Nizar951 <nizarmayraldo@gmail.com>
2021-06-18 20:41:25 +0700

    library

---
+++ client/program_dump_client.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
